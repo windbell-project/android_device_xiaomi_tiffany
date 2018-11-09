80d4935704bb4f09ec7194eac0702a475f6079b7
soshiweekly <1376106928@qq.com>
2018-11-09 19:27:15 +0800

    tiffany: fix copying of sensor_def_qcomdev.conf
    Change-Id: Ie627cd4c0599e86d0e591293f8a6930fddbc3af8

--- device.mk
+++ device.mk
@@ -42,6 +42,6 @@ PRODUCT_PACKAGES += \
     init.tiffany.rc
 
 # Sensors
-PRODUCT_PACKAGES += \
+PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/sensors/sensor_def_qcomdev.conf:$(TARGET_COPY_OUT_VENDOR)/etc/sensors/sensor_def_qcomdev.conf
 
