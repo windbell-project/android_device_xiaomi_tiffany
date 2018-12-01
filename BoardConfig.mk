67487235b8393aa7c831a0662f336a59b396489a
Artem Borisov <dedsa2002@gmail.com>
2018-12-01 15:05:05 +0400

    tiffany: Remove extra root folders
    Mount points have changed in P CAF, so we don't need these anymore.

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -38,9 +38,6 @@ TARGET_TAP_TO_WAKE_NODE := "/proc/touchpanel/enable_dt2w"
 # Properties
 TARGET_SYSTEM_PROP := $(DEVICE_PATH)/system.prop
 
-# Root Folders
-BOARD_ROOT_EXTRA_FOLDERS := dsp firmware persist
-
 # Security Patch Level
 VENDOR_SECURITY_PATCH := 2018-10-05
 
