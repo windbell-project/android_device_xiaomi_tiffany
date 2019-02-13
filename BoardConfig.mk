bb563e0b5f0fd512fd9b80d37f9a074b424bffd2
Artem Borisov <dedsa2002@gmail.com>
2019-02-13 12:45:16 +0530

    tiffany: camera: Re-enable HAL1 support
    * And disable dual camera muxer to make it initialize properly
    
    Change-Id: I71e2229a8f7c1d39640796f1013dcfa38b64b158

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -18,9 +18,6 @@
 
 DEVICE_PATH := device/xiaomi/tiffany
 
-# Camera
-TARGET_SUPPORT_HAL1 := false
-
 # Filesystem
 BOARD_CACHEIMAGE_FILE_SYSTEM_TYPE := ext4
 BOARD_VENDORIMAGE_FILE_SYSTEM_TYPE := ext4
