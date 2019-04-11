287b340ee6189e6d9bb6a333863d121b2f010d69
Artem Borisov <dedsa2002@gmail.com>
2019-04-11 17:26:45 +0530

    tiffany: Add dalvik property overrides * Yes, we already handle this in our init extension, but   it can't be used with treble.
    Change-Id: Ibb31ae9fe5c8d547cd12f329cb694af463eace55

--- prop.mk
+++ prop.mk
@@ -12,3 +12,12 @@ persist.camera.dual.camera=0
 
 # Radio
 persist.radio.aosp_usr_pref_sel=true
+
+# Dalvik
+PRODUCT_PROPERTY_OVERRIDES += \
+dalvik.vm.heapstartsize=16m \
+dalvik.vm.heapgrowthlimit=256m \
+dalvik.vm.heapsize=512m \
+dalvik.vm.heaptargetutilization=0.75 \
+dalvik.vm.heapminfree=4m \
+dalvik.vm.heapmaxfree=8m \
