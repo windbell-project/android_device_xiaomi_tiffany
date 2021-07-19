bfa8bbe7b004cff09238241f2ee0c7a1ccd66c38
Sebastiano Barezzi <barezzisebastiano@gmail.com>
2021-07-19 12:35:06 +0200

    tiffany: Decommonize keymaster and gatekeeper
    Change-Id: Id560a401a61fd6c56cd15940a40720a67b7d2a82

--- device.mk
+++ device.mk
@@ -42,6 +42,16 @@ PRODUCT_COPY_FILES += \
 # Firmware
 $(call inherit-product, vendor/xiaomi/firmware/tiffany/firmware.mk)
 
+# Gatekeeper
+PRODUCT_PACKAGES += \
+    android.hardware.gatekeeper@1.0-impl \
+    android.hardware.gatekeeper@1.0-service
+
+# Keymaster
+PRODUCT_PACKAGES += \
+    android.hardware.keymaster@3.0-impl \
+    android.hardware.keymaster@3.0-service
+
 # Properties
 -include device/xiaomi/tiffany/prop.mk
 
