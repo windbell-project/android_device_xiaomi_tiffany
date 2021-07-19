0bed8c0365ccc42ee60502f09e5b316e197eea96
Sebastiano Barezzi <barezzisebastiano@gmail.com>
2021-07-19 09:43:00 +0200

    tiffany: Decommonize fingerprint permission XML
    Change-Id: I66213a2b79293816b5e55a21291d7e3720c89df0

--- device.mk
+++ device.mk
@@ -36,6 +36,8 @@ PRODUCT_PACKAGES += \
 # Fingerprint
 PRODUCT_PACKAGES += \
     android.hardware.biometrics.fingerprint@2.1-service.xiaomi_msm8953
+PRODUCT_COPY_FILES += \
+    frameworks/native/data/etc/android.hardware.fingerprint.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.fingerprint.xml
 
 # Firmware
 $(call inherit-product, vendor/xiaomi/firmware/tiffany/firmware.mk)
