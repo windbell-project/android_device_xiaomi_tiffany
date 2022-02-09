5ae62e77f951138647cdc50d4ba9d634c4513c3f
mikanxx <1376106928@qq.com>
2022-02-09 20:55:07 +0800

    tiffany: Improve inheritting order

--- aosp_tiffany.mk
+++ aosp_tiffany.mk
@@ -18,14 +18,14 @@
 $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 
-# Inherit from tiffany device
-$(call inherit-product, device/xiaomi/tiffany/device.mk)
-
 # Inherit some common Lineage stuff.
 $(call inherit-product, vendor/aosp/common.mk)
 
 # Aosp Extended Properties
 TARGET_BOOT_ANIMATION_RES := 1080
+
+# Inherit from tiffany device
+$(call inherit-product, device/xiaomi/tiffany/device.mk)
 
 # Device identifier. This must come after all inclusions
 PRODUCT_DEVICE := tiffany
